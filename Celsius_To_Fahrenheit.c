b691e884d334e956fb7d0798b4bfcb15cb562377
Shubham Das <85635405+Shubham-Das-Coder@users.noreply.github.com>
2021-12-22 19:08:40 +0530

    Add files via upload

---
+++ Celsius_To_Fahrenheit.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(int argc, char const *argv[])
+{
+    float c,f;
+    printf("Please Enter The Temperature In Celsius:");
+    scanf("%f",&c);
+    f=(1.8*c)+32;
+    printf("Temperature In Fahrenheit Is %f",f);
+    return 0;
+}
