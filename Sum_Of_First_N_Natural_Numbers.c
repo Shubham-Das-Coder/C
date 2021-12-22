1f43ade812b2554e3b199543409b748ede21cc18
Shubham Das <85635405+Shubham-Das-Coder@users.noreply.github.com>
2021-12-22 22:16:23 +0530

    Add files via upload

---
+++ Sum_Of_First_N_Natural_Numbers.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main(int argc, char const *argv[])
+{
+    int num, sum = 0;
+    printf("Please Enter A Natural Number\n");
+    scanf("%d", &num);
+    for (int i = 0; i <= num; i++)
+    {
+        sum = sum + i;
+    }
+    printf("Sum Of First %d Natural Numbers Is : %d", num, sum);
+    return 0;
+}
