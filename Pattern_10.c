e2896c8294a88318f6b5caaa8e070376892e8870
Shubham Das <85635405+Shubham-Das-Coder@users.noreply.github.com>
2021-12-22 22:57:38 +0530

    Add files via upload

---
+++ Pattern_10.c
@@ -0,0 +1,21 @@
+/*
+112131
+122232
+132333
+142434
+152535
+*/
+#include <stdio.h>
+int main(int argc, char const *argv[])
+{
+    int i, j;
+    for (i = 1; i <= 5; i++)
+    {
+        for (j = 1; j <= 3; j++)
+        {
+            printf("%d%d", j, i);
+        }
+        printf("\n");
+    }
+    return 0;
+}
