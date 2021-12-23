e806392edf277084685620323ec6d0cd66a7def5
Shubham Das <85635405+Shubham-Das-Coder@users.noreply.github.com>
2021-12-23 17:13:55 +0530

    Add files via upload

---
+++ Pattern_29.c
@@ -0,0 +1,21 @@
+/*
+EDCBA
+EDCBA
+EDCBA
+EDCBA
+EDCBA
+*/
+#include <stdio.h>
+int main(int argc, char const *argv[])
+{
+    char i, j;
+    for (i = 'E'; i >= 'A'; i--)
+    {
+        for (j = 'E'; j >= 'A'; j--)
+        {
+            printf("%c", j);
+        }
+        printf("\n");
+    }
+    return 0;
+}
