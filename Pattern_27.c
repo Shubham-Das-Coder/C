289db4404693dee3ffcf5795fd1340a1b40179a5
Shubham Das <85635405+Shubham-Das-Coder@users.noreply.github.com>
2021-12-23 17:11:40 +0530

    Add files via upload

---
+++ Pattern_27.c
@@ -0,0 +1,21 @@
+/*
+ABCDE
+ABCDE
+ABCDE
+ABCDE
+ABCDE
+*/
+#include <stdio.h>
+int main(int argc, char const *argv[])
+{
+    char i, j;
+    for (i = 'A'; i <= 'E'; i++)
+    {
+        for (j = 'A'; j <= 'E'; j++)
+        {
+            printf("%c", j);
+        }
+        printf("\n");
+    }
+    return 0;
+}
