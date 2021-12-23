c0b17a6829b8d9a0aae9bb9cae11d7af1e62b0a4
Shubham Das <85635405+Shubham-Das-Coder@users.noreply.github.com>
2021-12-23 15:08:55 +0530

    Add files via upload

---
+++ Pattern_22.c
@@ -0,0 +1,22 @@
+/*
+ 0 0 0 0 0
+ 1 1 1 1 1
+ 0 0 0 0 0
+ 1 1 1 1 1
+ 0 0 0 0 0
+*/
+#include <stdio.h>
+int main(int argc, char const *argv[])
+{
+    int i, j;
+    int n = 5;
+    for (i = 0; i < n; i++)
+    {
+        for (j = 1; j <= n; j++)
+        {
+            printf("%2d", i % 2);
+        }
+        printf("\n");
+    }
+    return 0;
+}
