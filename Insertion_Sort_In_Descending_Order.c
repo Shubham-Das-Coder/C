8da6cab5c2b1d3111724e0ab0f086f1b7c51e611
Shubham Das <85635405+Shubham-Das-Coder@users.noreply.github.com>
2022-08-20 10:48:30 +0530

    Add files via upload

---
+++ Insertion_Sort_In_Descending_Order.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+void insertionSort(int arr[], int n)
+{
+    int i, j, key;
+    for (i = 1; i < n; i++)
+    {
+        key = arr[i];
+        j = i - 1;
+        while (j >= 0 && arr[j] < key)
+        {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+        arr[j + 1] = key;
+    }
+}
+
+void printArray(int arr[], int n)
+{
+    int i;
+    printf("The array in descending order is:\n");
+    for (i = 0; i < n; i++)
+        printf("%d\t", arr[i]);
+}
+
+int main()
+{
+    int arr[100], n, i;
+    printf("Enter the number of elements: ");
+    scanf("%d", &n);
+    printf("Enter the elements:\n");
+    for (i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+    insertionSort(arr, n);
+    printArray(arr, n);
+    return 0;
+}
